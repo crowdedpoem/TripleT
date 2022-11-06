f4b188b11b5786fd867461e71b977bfad7b48287
Tpanyasr <tpanyasr@gmail.com>
2022-11-06 07:42:19 -0800

    push

--- frontend/src/RecipeProp.js
+++ frontend/src/RecipeProp.js
@@ -3,31 +3,58 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
+// function RecipeProp(props) {
+//     // <TableBody populateCard = {props.populateCard} />
+//     const recipedata = props.passdata.map((recipe) => {
+//       return (
+//         <div key={recipe.id}>
+
+//                 <Card.Title>{recipe.title}</Card.Title>
+//                 <Card.Subtitle>{recipe.time}</Card.Subtitle>
+//                 <Card.Subtitle>{recipe.cost}</Card.Subtitle>
+//                 <Card.Text>{recipe.description}</Card.Text>
+
+//         </div>
+//       );
+//     });
+//     return(
+//         <CardGroup>
+//         <Card style={{ display: "inline-block" }}>
+//           <Card.Body>
+//              {recipedata}
+//              <Button variant="primary">See Recipe</Button>
+//           </Card.Body>
+//             </Card>
+//           </CardGroup>
+//     );
+
+// }
+
 function RecipeProp(props) {
-    // <TableBody populateCard = {props.populateCard} />
-    const data = props.map((recipe) => {
-      return (
-        <div key={recipe.id}>
+  // <TableBody populateCard = {props.populateCard} />
+  const recipe = props.passdata;
+  return(
+      <CardGroup>
+      <Card style={{ display: "inline-block" }}>
+        <Card.Body>
+        <Card.Title>{recipe.title}</Card.Title>
+              <Card.Subtitle>{recipe.time}</Card.Subtitle>
+              <Card.Subtitle>{recipe.cost}</Card.Subtitle>
+              <Card.Text>{recipe.description}</Card.Text>
 
-                <Card.Title>{recipe.title}</Card.Title>
-                <Card.Subtitle>{recipe.time}</Card.Subtitle>
-                <Card.Subtitle>{recipe.cost}</Card.Subtitle>
-                <Card.Text>{recipe.description}</Card.Text>
-
-        </div>
-      );
-    });
-    return(
-        <CardGroup>
-        <Card style={{ display: "inline-block" }}>
-          <Card.Body>
-             {data}
-             <Button variant="primary">See Recipe</Button>
-          </Card.Body>
-            </Card>
-          </CardGroup>
-    );
+           <Button variant="primary">See Recipe</Button>
+        </Card.Body>
+          </Card>
+        </CardGroup>
+  );
 
 }
 
-export default RecipeProp;
+function Recipe(props)
+{
+  return(
+    <RecipeProp passdata = {props.passdata}/>
+  )
+}
+
+export default Recipe;
