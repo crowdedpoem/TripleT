26e624b6c84ca1911834bdd305f48007392bed6a
tpanyasr <tpanyasr@gmail.com>
2022-11-06 19:58:08 -0800

    done with populating cards

--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -1,68 +1,75 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import RecipeCard from "./RecipeCards";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Form from "./Form";
+import RecipePage from "./RecipePage";
 import Recipe from "./RecipeProp";
-
-// import Button from "react-bootstrap/Button";
-// // import Card from "react-bootstrap/Card";
-// import CardGroup from "react-bootstrap/CardGroup";
-
-// import RecipeCard from "./card.js"
 import "bootstrap/dist/css/bootstrap.min.css";
+import CardGroup from "react-bootstrap/CardGroup";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
+  const [recipes_list, setRecipe] = useState([]);
+  useEffect(() => {
+    fetchAll().then((result) => {
+      if (result) setRecipe(result);
+    });
+  }, []);
 
-  const recipes = [
-    {
-      id: 1,
-      title: "Mac n Cheese",
-      time: "Total Time: 45",
-      cost: "Cost per Serving: $2.50",
-      description: "Easy to make recipe, full of carbs",
-    },
-    {
-      id: 2,
-      title: "Baked Potatoes",
-      time: "Total Time: 25",
-      cost: "Cost per Serving: $1.50",
-      description: "Easy to make recipe, full of carbs",
-    },
-  ];
+  async function fetchAll() {
+    try {
+      const response = await axios.get("http://localhost:5000/recipes");
+      return response.data.recipes_list;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
 
-  return (
-    <div className="container">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">RecipeBuddy</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/inputRecipe">Add a recipe</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+  async function fetchByID(id) {
+    try {
+      const response = await axios.get("http://localhost:5000/recipes/" + id);
+      return response.data.recipes_list;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="inputRecipe" element={<InputRecipe />} />
-      </Routes>
-    </div>
-  );
+  if (recipes_list.length !== 0) {
+    return (
+      <div className="container">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">RecipeBuddy</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/inputRecipe">Add a recipe</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="inputRecipe" element={<InputRecipe />} />
+          <Route path="/page" element={<RecipePage />} />
+        </Routes>
+      </div>
+    );
+  }
 
   function updateList(person) {
-    console.log("got to lien 33 of myapp.js");
     makePostCall(person).then((result) => {
       if (result && result.status === 201) {
         console.log("hello " + result.status);
-        setCharacters([...characters, result.data]);
+        setRecipe([...recipes_list, result.data]);
       }
     });
   }
@@ -93,39 +100,24 @@ function MyApp() {
   //   });
   // }, []);
 
-  async function fetchAll() {
-    try {
-      const response = await axios.get("http://localhost:5000/users");
-      return response.data.recipes_list;
-    } catch (error) {
-      //We're not handling errors. Just logging into the console.
-      console.log(error);
-      return false;
-    }
-  }
-
   function Home() {
     return (
-      <div>
+      <div className="main-container">
         <main>
-          <h2>Welcome to the homepage!</h2>
-          <p>Here are some interesting recipes to check out</p>
-
-          {/* <Table characterData={characters} removeCharacter={removeOneCharacter} /> */}
+          <h2>Welcome to RecipeBuddy!</h2>
+          <p>Here are some interesting recipes to explore!</p>
         </main>
-        <nav>{/* <Link to="/inputRecipe">About</Link> */}</nav>
-        <div className="container">
-          <Recipe passdata={recipes[1]} />
-          <RecipeCard
-            title="Mac n Cheese"
-            time="Total Time: 45"
-            cost="Cost per Serving: $2.50"
-            description="Easy to make recipe, full of carbs"
-          />
+        <div className="cardgroup-container">
+          <CardGroup>
+            <Recipe passdata={recipes_list[0]} />
+            <Recipe passdata={recipes_list[1]} />
+            <Recipe passdata={recipes_list[2]} />
+          </CardGroup>
         </div>
       </div>
     );
   }
+
   function InputRecipe() {
     return (
       <>
